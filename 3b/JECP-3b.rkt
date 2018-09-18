2f37863f8ee2b9d05fb64d33e8ccb281bffabc97
Casey Pancoast <cqpancoast@gmail.com>
2018-09-18 16:12:52 -0400

    Next edit
    Added check-expects for everything that needed it, changed some of the examples to be better/more varied for testing, changed INTERVAL-HEIGHT to GOAL-HEIGHT; left notes for you at the bottom.

--- 3b/JECP-3b.rkt
+++ 3b/JECP-3b.rkt
@@ -12,7 +12,7 @@
 (define i3 (make-interval 0 12))
 ; Template:
 #;(define (interval-temp i)
-  (... (interval-left i) ... (interval-right i) ...))
+    (... (interval-left i) ... (interval-right i) ...))
 
 
 ; A DS (Drone Shoot) is one of:
@@ -27,19 +27,19 @@
 ; All numbers are measured in pixels.
 ; Examples:
 (define ds1 (make-launch 3 (make-interval 12 24)))
-(define ds2 (make-launch 0 (make-interval 1 2)))
-(define ds3 (make-flight 1 (make-interval 20 200) (make-posn 1 2)))
-(define ds4 (make-flight 12 (make-interval 10 15) (make-posn 3 0)))
+(define ds2 (make-launch 0 (make-interval 50 100)))
+(define ds3 (make-flight 100 (make-interval 20 200) (make-posn 10 20)))
+(define ds4 (make-flight 12 (make-interval 190 210) (make-posn 0 0)))
 ; Template
 #;(define (drone-shoot-temp ds)
-  (cond
-    [(launch? ds)
-         (... (launch-photographer ds)
-          ... (interval-temp (launch-goal ds)) ...)]
-    [(flight? ds)
-         (... (flight-photographer ds) ...
-          ... (interval-temp (flight-goal ds)) ...
-          ... (flight-drone ds) ...)]))
+    (cond
+      [(launch? ds)
+       (... (launch-photographer ds)
+            ... (interval-temp (launch-goal ds)) ...)]
+      [(flight? ds)
+       (... (flight-photographer ds) ...
+            ... (interval-temp (flight-goal ds)) ...
+            ... (flight-drone ds) ...)]))
 
 
 ; falling-drone : DS -> DS
@@ -56,9 +56,9 @@
             (posn-y (flight-drone ds)))))
       ds))
 
-(check-expect (falling-drone ds1) ds1)
-(check-expect (falling-drone ds3) (make-flight 1 (make-interval 20 200) (make-posn 1 1)))
-(check-expect (falling-drone ds4) ds4)
+(check-expect (falling-drone ds1) ds1) ;There should be no change as there is no drone
+(check-expect (falling-drone ds3) (make-flight 100 (make-interval 20 200) (make-posn 10 19)))
+(check-expect (falling-drone ds4) ds4) ;No change, as the drone is already on the ground.
 
 
 ; launch-drone : DS -> DS
@@ -75,7 +75,7 @@
  (make-flight 3 (make-interval 12 24) (make-posn 18 20)))
 (check-expect
  (launch-drone ds2)
- (make-flight 0 (make-interval 1 2) (make-posn 15 20)))
+ (make-flight 0 (make-interval 50 100) (make-posn 15 20)))
 (check-expect
  (launch-drone ds3)
  ds3)
@@ -84,19 +84,19 @@
 ; shoot-over? : DS -> Boolean
 ; checks if the drone has shot the whole goal (or the whole goal plus some extra), or has crashed.
 (define (shoot-over? ds)
-      (cond
-        [(launch? ds) #false]
-        [(= (posn-y (flight-drone ds)) 0) #true]
-        [(and
-          (<= (-
-               (posn-x (flight-drone ds))
-               (/ (posn-y (flight-drone ds)) 2))
-              (interval-left (flight-goal ds)))
-          (>= (+
-               (posn-x (flight-drone ds))
-               (/ (posn-y (flight-drone ds)) 2))
-              (interval-right (flight-goal ds)))) #true]
-        [else #false]))
+  (cond
+    [(launch? ds) #false]
+    [(= (posn-y (flight-drone ds)) 0) #true]
+    [(and
+      (<= (-
+           (posn-x (flight-drone ds))
+           (/ (posn-y (flight-drone ds)) 2))
+          (interval-left (flight-goal ds)))
+      (>= (+
+           (posn-x (flight-drone ds))
+           (/ (posn-y (flight-drone ds)) 2))
+          (interval-right (flight-goal ds)))) #true]
+    [else #false]))
 
 (check-expect
  (shoot-over? (make-flight 2 (make-interval 10 20) (make-posn 5 5)))
@@ -120,7 +120,7 @@
 (define HEIGHT 100)
 (define WIDTH 600)
 (define PHOTOG-HEIGHT 20)
-(define INTERVAL-HEIGHT 4)
+(define GOAL-HEIGHT 4)
 (define DRONE-HEIGHT 10)
 (define SCENE (empty-scene WIDTH HEIGHT))
 (define PHOTOG (rectangle 5 PHOTOG-HEIGHT "solid" "chartreuse"))
@@ -130,40 +130,47 @@
 ; Makes an image of the current drone flying state.
 (define (draw-ds ds)
   (cond
-
     [(launch? ds) (underlay/xy (underlay/xy SCENE
                                             (launch-photographer ds)
                                             (- HEIGHT PHOTOG-HEIGHT)
                                             PHOTOG)
                                (interval-left (launch-goal ds))
-                               (- HEIGHT INTERVAL-HEIGHT)
+                               (- HEIGHT GOAL-HEIGHT)
                                (goal (launch-goal ds)))]
-
     [(flight? ds) (underlay/xy (underlay/xy (underlay/xy SCENE
-                                            (flight-photographer ds)
-                                            (- HEIGHT PHOTOG-HEIGHT)
-                                            PHOTOG)
-                               (interval-left (flight-goal ds))
-                               (- HEIGHT INTERVAL-HEIGHT)
-                               (goal (flight-goal ds)))
-                                   (posn-x (flight-drone ds))
-                                   (- HEIGHT (posn-y (flight-drone ds)) DRONE-HEIGHT)
-                                   DRONE)]))
+                                                         (flight-photographer ds)
+                                                         (- HEIGHT PHOTOG-HEIGHT)
+                                                         PHOTOG)
+                                            (interval-left (flight-goal ds))
+                                            (- HEIGHT GOAL-HEIGHT)
+                                            (goal (flight-goal ds)))
+                               (posn-x (flight-drone ds))
+                               (- HEIGHT (posn-y (flight-drone ds)) DRONE-HEIGHT)
+                               DRONE)]))
 
-(check-expect (draw-ds ds1) (overlay/xy PHOTOG
-                            3
-                            (- HEIGHT PHOTOG-HEIGHT)
-                            (overlay/xy (goal (make-interval 12 24))
-                                        12
-                                        (- HEIGHT INTERVAL-HEIGHT)
-                                        SCENE)))
-(check-expect (draw-ds ds3) (rectangle 12 20 "solid" "green"))
+(check-expect (draw-ds ds1) (underlay/xy (underlay/xy SCENE
+                                                      3
+                                                      (- HEIGHT PHOTOG-HEIGHT)
+                                                      PHOTOG)
+                                         12
+                                         (- HEIGHT GOAL-HEIGHT)
+                                         (rectangle 12 GOAL-HEIGHT "solid" "gold")))
+(check-expect (draw-ds ds3) (underlay/xy (underlay/xy (underlay/xy SCENE
+                                                                   100
+                                                                   (- HEIGHT PHOTOG-HEIGHT)
+                                                                   PHOTOG)
+                                                      20
+                                                      (- HEIGHT GOAL-HEIGHT)
+                                                      (rectangle 180 GOAL-HEIGHT "solid" "gold"))
+                                         10
+                                         (- HEIGHT 20 DRONE-HEIGHT)
+                                         DRONE))
 
 
 ; goal : Interval -> Image
 ; Creates a visual representation of the goal described by the interval.
 (define (goal interval)
-  (rectangle (- (interval-right interval) (interval-left interval)) INTERVAL-HEIGHT "solid" "gold"))
+  (rectangle (- (interval-right interval) (interval-left interval)) GOAL-HEIGHT "solid" "gold"))
 
 (check-expect (goal (make-interval 2.2 4.2)) (rectangle 2 4 "solid" "gold"))
 (check-expect (goal (make-interval -3 2)) (rectangle 5 4 "solid" "gold"))
@@ -175,30 +182,36 @@
   (cond
     [(and (launch? ds) (string=? "l" ke)) (launch-drone ds)]
     [(and (flight? ds) (string=? "up" ke)) (make-flight (flight-photographer ds)
-                                     (flight-goal ds)
-                                     (make-posn
-                                      (posn-x (flight-drone ds))
-                                      (+ (posn-y (flight-drone ds)) 5)))]
+                                                        (flight-goal ds)
+                                                        (make-posn
+                                                         (posn-x (flight-drone ds))
+                                                         (+ (posn-y (flight-drone ds)) 5)))]
     [(and (flight? ds) (string=? "left" ke)) (make-flight (flight-photographer ds)
-                                     (flight-goal ds)
-                                     (make-posn
-                                      (- (posn-x (flight-drone ds)) 5)
-                                      (posn-y (flight-drone ds))))]
+                                                          (flight-goal ds)
+                                                          (make-posn
+                                                           (- (posn-x (flight-drone ds)) 5)
+                                                           (posn-y (flight-drone ds))))]
     [(and (flight? ds) (string=? "right" ke)) (make-flight (flight-photographer ds)
-                                     (flight-goal ds)
-                                     (make-posn
-                                      (+ (posn-x (flight-drone ds)) 5)
-                                      (posn-y (flight-drone ds))))]
+                                                           (flight-goal ds)
+                                                           (make-posn
+                                                            (+ (posn-x (flight-drone ds)) 5)
+                                                            (posn-y (flight-drone ds))))]
     [else ds]))
 
-;; WRITER RESTSETSEASALTjrsahjgkqbda sga
+(check-expect (user-input ds1 "l") (make-flight 3 (make-interval 12 24) (make-posn (+ 3 15) 20)))
+(check-expect (user-input ds3 "up") (make-flight 100 (make-interval 20 200) (make-posn 10 25)))
+(check-expect (user-input ds3 "right") (make-flight 100 (make-interval 20 200) (make-posn 15 20)))
+(check-expect (user-input ds3 "left") (make-flight 100 (make-interval 20 200) (make-posn 5 20)))
+(check-expect (user-input ds1 "left") ds1)
 
 
-; main : DS -> Boolean
+; main : DS -> DS
 ; Runs a drone photography game and outputs whether or not it was a success.
 (define (main ds)
   (big-bang ds
-    [on-tick falling-drone 10]
+    [on-tick falling-drone .1]
     [to-draw draw-ds]
     [on-key user-input]
-    [stop-when shoot-over?]))
+    [stop-when shoot-over?])) ;Hey Jesse -- Can't figure out how to make this return a boolean.
+                              ;Also, do we have to write tests for this?
+
