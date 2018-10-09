fe4fd60cfda545b4c5feadf16d236195dec89eff
Jesse Evers <jesse27999@gmail.com>
2018-10-09 12:33:19 -0400

    added 6a

--- 6a/6a.rkt
+++ 6a/6a.rkt
@@ -1,6 +1,6 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-intermediate-reader.ss" "lang")((modname 5a) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-intermediate-reader.ss" "lang")((modname 6a) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;
 ; Problem Set 5a and 6b ;
 ;;;;;;;;;;;;;;;;;;;;;;;;;
